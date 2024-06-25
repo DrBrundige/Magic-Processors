94981b1c937c16b3a6658b23b0e4a10d881c7ffe
thomasj@saltustech.com <brundigejones@gmail.com>
2024-06-25 08:58:51 -0500

    Updates get all creature types
    Updates the get_all_creature_types method with the latest comprehensive rules update.

--- src/magic_processor_03_histogram.py
+++ src/magic_processor_03_histogram.py
@@ -2,7 +2,7 @@ from magic_processor_03 import get_all_cards_from_data_file, get_cards_from_api,
 from common_methods_io import *
 from common_methods_processor import *
 from import_scryfall_bulk_data import *
-from common_methods_processor_03 import get_usd_from_card_03, get_price_range_03
+from common_methods_processor_03 import get_usd_from_card_03, get_price_range_03, get_all_creature_types
 from common_methods_call_scryfall import *
 from datetime import *
 from magic_sorter_03 import MagicSorterTrie03
@@ -35,7 +35,8 @@ def output_histogram(all_cards, match_field, histogram=None, do_split_faces=Fals
 
 # Accepts a list of new_card objects, a single match field, and a histogram
 def custom_histogram_creature_types(all_cards):
-	all_creature_types = controller_get_scryfall_creature_types()
+	# all_creature_types = controller_get_scryfall_creature_types()
+	all_creature_types = get_all_creature_types()
 	histogram = {}
 	for creature_type in all_creature_types:
 		histogram[creature_type] = 0
@@ -57,8 +58,8 @@ def controller_process_histogram_from_data_file(data, match_field, output_name="
 
 def controller_process_histogram_from_api(request_url, match_field, output_name="scan"):
 	all_cards = get_cards_from_api(request_url)
-	populated_histogram = output_histogram(all_cards, match_field)
-	# populated_histogram = custom_histogram_creature_types(all_cards)
+	# populated_histogram = output_histogram(all_cards, match_field)
+	populated_histogram = custom_histogram_creature_types(all_cards)
 	write_data_dictionary(populated_histogram, filename=output_name)
 
 
@@ -70,14 +71,14 @@ def controller_process_histogram_from_sheet(filename, data, match_field, output_
 
 if __name__ == '__main__':
 	# data = import_scryfall("eternal-cards")
-	data = controller_get_sorted_data()
+	# data = controller_get_sorted_data()
 	# filename = "audit_csv.csv"
 	# request_url = get_set_search_uri_from_set_code("OTJ")
 	# set = "mkm"
 	# request_url = f"https://api.scryfall.com/cards/search?include_extras=true&order=set&q=e%3A{set}+game%3Apaper"
-	# request_url = "https://api.scryfall.com/cards/search?include_extras=true&order=set&" \
-	#               "q=type%3Acreature+type%3Aelder+game%3Apaper+f%3Avintage&unique=cards&as=grid&order=name"
+	request_url = "https://api.scryfall.com/cards/search?include_extras=true&order=set&" \
+	              "q=type%3Acreature+type%3Asliver+game%3Apaper+f%3Avintage&unique=cards&as=grid&order=name"
 	match_field = "mana_cost"
 	# controller_process_histogram_from_sheet(filename, data, match_field)
-	# controller_process_histogram_from_api(request_url, match_field)
-	controller_process_histogram_from_data_file(data, match_field, do_split_faces=True)
+	controller_process_histogram_from_api(request_url, match_field)
+	# controller_process_histogram_from_data_file(data, match_field, do_split_faces=True)
